03a92b47b4bf2d491e9ae28d54a9f69d4beafe10
mundialresponse <contato@mundialresponse.com>
2023-09-22 12:58:52 -0300

    subindo video de pedro paulo

--- src/app/pedro-paulo-rolemberg/page.jsx
+++ src/app/pedro-paulo-rolemberg/page.jsx
@@ -5,7 +5,9 @@ export default function Home() {
     return(
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
-                <video controls src="convitepedropaulo.mp4"></video>
+            <video controls poster="bannerpedropaulo.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/meusite-32bd9.appspot.com/o/convitepedropaulo.mp4?alt=media&token=ce58c863-5536-41a9-b199-adbb2dc9b875" type="video/mp4" />
+                </video>    
             </div>
         </main>
     )
