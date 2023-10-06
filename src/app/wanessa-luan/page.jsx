e430df11e57b8214dd87a6853a2c934683df263f
mundialresponse <contato@mundialresponse.com>
2023-10-06 15:54:18 -0300

    video de wanessa e luan

---
+++ src/app/wanessa-luan/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+            <video controls poster="wanessa.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/meusite-32bd9.appspot.com/o/_Convite%20v%C3%ADdeo_wanessa.mp4?alt=media&token=9d34266f-fd0a-4d1d-8a9e-735b16a07983&_gl=1*dvfeks*_ga*MTMxMjUzNzcxNS4xNjk1MjI0MDYx*_ga_CW55HF8NVT*MTY5NjYxNzgwMy4xMy4xLjE2OTY2MTc5NzQuNjAuMC4w" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
