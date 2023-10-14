ca6bf232c89df0812e5f07c84f59240ab48767d6
lucasalbertolima <lucas.alberto.san@gmail.com>
2023-10-14 10:49:53 -0300

    add page e video patricia e cristian

---
+++ src/app/patricia-e-cristian/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+            <video controls poster="patricia-cristian.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-video-2.appspot.com/o/Convite%20Especial_Patr%C3%ADcia%20%26%20Cristian%20-%20v%C3%ADdeo.mp4?alt=media&token=7ef636d9-dce6-43a5-a4a4-c36b5820500c&_gl=1*ikfn62*_ga*MTc5MjIyMTkzLjE2ODMzMDk1ODg.*_ga_CW55HF8NVT*MTY5NzI5MTAxMy4zNS4xLjE2OTcyOTEyMzEuNjAuMC4w" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
