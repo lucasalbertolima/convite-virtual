c4530e2882be146a2d557edf309c51079d677df9
lucasalbertolima <lucas.alberto.san@gmail.com>
2023-10-14 09:14:25 -0300

    add page e video do ravi

---
+++ src/app/ravi/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+            <video controls poster="ravi.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-video.appspot.com/o/video.mp4?alt=media&token=0cb4aae4-ad99-40f2-8c62-58c8a2be72db&_gl=1*vkvj0v*_ga*MTc5MjIyMTkzLjE2ODMzMDk1ODg.*_ga_CW55HF8NVT*MTY5NzI4NDg5Ni4zNC4xLjE2OTcyODU0MjUuMTUuMC4w" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
