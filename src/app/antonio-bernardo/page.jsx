f0719e79b9c1ce593874955f39f075ca54069bbc
mundialresponse <contato@mundialresponse.com>
2023-10-18 13:16:44 -0300

    subindo video de tomtom

---
+++ src/app/antonio-bernardo/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+            <video controls poster="bannertomtom.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-mari-andre.appspot.com/o/Convite_especial_tom%20tom.mp4?alt=media&token=77c62627-159c-471f-b1ca-89316f23298c&_gl=1*17p0g67*_ga*ODc4ODIyNTM0LjE2OTU2NzA4MTI.*_ga_CW55HF8NVT*MTY5NzY0NDgwNC43LjEuMTY5NzY0NTUyMS4yNi4wLjA." type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
