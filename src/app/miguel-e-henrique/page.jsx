b17b74f78bd73957e86085833a63770e949522c7
mundialresponse <contato@mundialresponse.com>
2023-11-10 12:10:04 -0300

    commit pagina miguel e henrique

---
+++ src/app/miguel-e-henrique/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+            <video controls poster="miguel-e-henrique.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/gisela-jose.appspot.com/o/Convite%20especial.mp4?alt=media&token=dc85925e-2edf-40dc-af50-4c82e0544d92" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
