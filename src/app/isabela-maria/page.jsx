760d80daff8e309939d8b42d1fa88d71d607e559
mundialresponse <contato@mundialresponse.com>
2023-11-13 10:57:42 -0300

    subindo video de isabela maria

---
+++ src/app/isabela-maria/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+            <video controls poster="isabela-maria.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-digital-bysarahjandira.appspot.com/o/Um%20lindo%20jardim%20encantado.mp4?alt=media&token=07bc2e99-2f1b-4ed3-b16c-cd7141dbbc9d" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
