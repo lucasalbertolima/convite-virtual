8753319827af7fd746fc323cafbbc2d6642a7d83
lucasalbertolima <lucas.alberto.san@gmail.com>
2023-11-13 19:25:07 -0300

    pagina de martin

---
+++ src/app/martin/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+            <video controls poster="martin.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/michelle-thiago.appspot.com/o/Martin.mp4?alt=media&token=56026163-d9d0-4f67-83cf-e94af6820085" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
