173bdb904adb8a23900110bae41876b8e6d8fbc5
lucasalbertolima <lucas.alberto.san@gmail.com>
2023-11-14 08:57:03 -0300

    subindo pagina de maria eduarda

---
+++ src/app/maria-eduarda/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+            <video controls poster="maria-eduarda.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/apinoticias-with-next.appspot.com/o/202311140850.mp4?alt=media&token=5b183a65-b225-417d-96c3-d397d95dd475" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
