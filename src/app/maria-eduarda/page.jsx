e288c2eab6d7e524f0711123de0615c5cf6a1cef
lucasalbertolima <lucas.alberto.san@gmail.com>
2023-11-15 22:55:13 -0300

    atualizando video de maria eduarda

--- src/app/maria-eduarda/page.jsx
+++ src/app/maria-eduarda/page.jsx
@@ -5,8 +5,9 @@ export default function Home() {
     return(
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
-            <video controls poster="maria-eduarda.jpeg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/apinoticias-with-next.appspot.com/o/202311140850.mp4?alt=media&token=5b183a65-b225-417d-96c3-d397d95dd475" type="video/mp4" />
+                <video controls poster="maria-eduarda.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convitevirtual-7554a.appspot.com/o/video%20maria%20eduarda.mp4?alt=media&token=6cfe0bb9-bcca-46ef-9d2a-4b113ef10069" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/phd-global.appspot.com/o/video%20maria%20eduarda.mp4?alt=media&token=29925ba1-edf6-4ef7-8556-98c611db689b" type="video/mp4" />
                 </video>    
             </div>
         </main>
