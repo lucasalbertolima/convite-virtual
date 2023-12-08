3c584b3c6f15c64de049828fb0693971cb54a170
lucasalbertolima <lucas.alberto.san@gmail.com>
2023-12-08 09:11:13 -0300

    subindo página manuela e marco

--- src/app/manuela-marco/page.jsx
+++ src/app/manuela-marco/page.jsx
@@ -5,9 +5,9 @@ export default function Home() {
     return(
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
-                <video controls poster="adrianagilson.jpeg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-videos.appspot.com/o/Convite%20Especial_Adriana%20e%20Gilson%20(1).mp4?alt=media&token=ff3ee877-92b5-4e9d-b30a-d1f7ffa653d4" type="video/mp4" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-videos-2.appspot.com/o/Convite%20Especial_Adriana%20e%20Gilson%20(1).mp4?alt=media&token=8ce57e84-0402-4ca9-b53e-104816ec766c" type="video/mp4" />
+                <video controls poster="manuela-marco.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-3.appspot.com/o/Convite%20v%C3%ADdeo_MM.mp4?alt=media&token=e6f7a4dd-53c4-409b-999a-79d3165b8859" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-4.appspot.com/o/Convite%20v%C3%ADdeo_MM.mp4?alt=media&token=f31f41c7-1a0d-4121-8bb1-95bef5e182ae" type="video/mp4" />
                 </video>    
             </div>
         </main>
