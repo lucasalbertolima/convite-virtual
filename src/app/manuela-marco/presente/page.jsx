bb789065f491ade5136380a97e8fdf01a72c7e06
lucasalbertolima <lucas.alberto.san@gmail.com>
2023-12-08 10:20:12 -0300

    subindo página de presnete  manuela e marco

---
+++ src/app/manuela-marco/presente/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-3.appspot.com/o/WhatsApp%20Image%202023-12-08%20at%2010.11.11.jpeg?alt=media&token=6bcb3861-e51b-4052-bb35-b72e49117642"  />
+            </div>
+        </main>
+    )
+    
+}
