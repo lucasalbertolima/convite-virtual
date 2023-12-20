9bf5133730889bb65fdba6471bf63695d6a182ad
lucasalbertolima <lucas.alberto.san@gmail.com>
2023-12-20 10:49:13 -0300

    subindo pagina de murilo

---
+++ src/app/murilo/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="murilo.png">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-5.appspot.com/o/Convite_Murilo_fundo%20do%20mar%20(1).mp4?alt=media&token=63e170b8-238c-4179-ac91-6518b066b245" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-6.appspot.com/o/Convite_Murilo_fundo%20do%20mar%20(1).mp4?alt=media&token=87326bf5-03f6-4051-be6c-4b75e00b7e1a" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
