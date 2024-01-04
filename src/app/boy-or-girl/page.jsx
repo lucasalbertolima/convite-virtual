08a48863b9ae4774c17d3c639e657f1b581925cd
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-01-04 10:37:01 -0300

    subindo pagina de dayvid e boy-or-girl

---
+++ src/app/boy-or-girl/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+            <video controls poster="boyorgirl.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-8.appspot.com/o/Boy%20or%20Girl%20(1).mp4?alt=media&token=b615f7e5-684e-4891-9960-ccb360a3f595" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
---
+++ src/app/dayvid/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+            <video controls poster="dayvid.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-7.appspot.com/o/convite_david_27anos.mp4?alt=media&token=dfa6c717-8786-49ba-8c23-729ca3d32f45" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
