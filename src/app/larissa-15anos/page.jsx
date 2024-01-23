709cdfd3de9bf4a8c7a114d417f29b44174b5c00
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-01-23 10:01:55 -0300

    adicionando a pagina de larissa-15anos

---
+++ src/app/larissa-15anos/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="larissa-15anos.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-1.appspot.com/o/Convite%2015%20anos%20larissa.mp4?alt=media&token=0b13634f-0ac4-4802-a4a1-24a99f553b60" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-2.appspot.com/o/Convite%2015%20anos%20larissa.mp4?alt=media&token=7d0272fe-4177-4700-be37-0d0e5a7a92f1" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
