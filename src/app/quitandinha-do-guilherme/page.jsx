098ec856e507d74014adbaf26274e525950498e8
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-01-23 13:36:46 -0300

    adicionando a pagina de quitandinha do guilherme

---
+++ src/app/quitandinha-do-guilherme/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="quitandinha-do-guilherme.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-3-de741.appspot.com/o/Quitandinha%20do%20Guilherme.mp4?alt=media&token=b60787c1-8e79-4365-a3f8-6478fc9d01b4" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
