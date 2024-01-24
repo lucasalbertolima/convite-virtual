89a506d91948e6fb1b9a3e6f64e78859a80c2a45
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-01-24 13:43:53 -0300

    alterando video de larissa-15anos

--- src/app/larissa-15anos/page.jsx
+++ src/app/larissa-15anos/page.jsx
@@ -6,8 +6,8 @@ export default function Home() {
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
                 <video controls poster="larissa-15anos.jpg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-1.appspot.com/o/Convite%2015%20anos%20larissa.mp4?alt=media&token=0b13634f-0ac4-4802-a4a1-24a99f553b60" type="video/mp4" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-2.appspot.com/o/Convite%2015%20anos%20larissa.mp4?alt=media&token=7d0272fe-4177-4700-be37-0d0e5a7a92f1" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-1.appspot.com/o/Convite%2015%20anos%20larissa.mp4?alt=media&token=792e4f51-b6ce-446a-9f09-699f8555dd8f" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-2.appspot.com/o/Convite%2015%20anos%20larissa.mp4?alt=media&token=77297d36-4265-4e9c-98af-64f6e953e84d" type="video/mp4" />
                 </video>    
             </div>
         </main>
