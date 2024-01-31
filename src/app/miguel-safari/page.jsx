32f681345280b3b244f8a77e824668d642793e74
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-01-31 12:01:53 -0300

    alterando o video miguel-safari

--- src/app/miguel-safari/page.jsx
+++ src/app/miguel-safari/page.jsx
@@ -6,8 +6,8 @@ export default function Home() {
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
                 <video controls poster="miguel-safari.jpeg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-by-sarahjandira.appspot.com/o/convite%20Miguel.mp4?alt=media&token=58e72db2-3a3a-4951-8513-22dfa304471f" type="video/mp4" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-digital-bysarahjandira.appspot.com/o/convite%20Miguel.mp4?alt=media&token=0a3ad05e-fb20-41ff-98f8-cd43a1547de6" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-by-sarahjandira.appspot.com/o/convite%20Miguel%20(1).mp4?alt=media&token=55b0b4ec-073f-4735-8d03-729352520f9d" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-digital-bysarahjandira.appspot.com/o/convite%20Miguel%20(1).mp4?alt=media&token=4cbccc1c-6323-4c70-82f0-3a90c413fe06" type="video/mp4" />
                 </video>    
             </div>
         </main>
