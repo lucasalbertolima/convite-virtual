5f922faaaaac12f1d193069b9cfac2c031189604
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-02-01 14:11:58 -0300

    adicionando pagina juliana-rodrigo

---
+++ src/app/juliana-rodrigo/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="juliana-rodrigo.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-4-7d650.appspot.com/o/Convite%20v%C3%ADdeo_Juliana%20e%20Rodrigo%20(1).mp4?alt=media&token=47c6172b-23c3-488e-bcd9-c46d173693ad" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-5-cabe1.appspot.com/o/Convite%20v%C3%ADdeo_Juliana%20e%20Rodrigo%20(1).mp4?alt=media&token=c7375be5-d496-4b81-a076-f42371817909" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
