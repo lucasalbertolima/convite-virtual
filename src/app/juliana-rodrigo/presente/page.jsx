5bee37f1851ba7acabfe4f6a770169ab12ef67ca
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-02-01 15:33:18 -0300

    adicionando pagina juliana-rodrigo aba de presentes

---
+++ src/app/juliana-rodrigo/presente/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-1.appspot.com/o/WhatsApp%20Image%202024-02-01%20at%2015.30.45.jpeg?alt=media&token=5c35fcb5-b86a-4a3a-bf6a-727a237fc7ee"  />
+            </div>
+        </main>
+    )
+    
+}
