d1601d61d4d62c6d2b36aca7a5046c7dbfa98e02
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-02-16 17:32:24 -0300

    subindo pagina de presentes de sophia

---
+++ src/app/sophia/presente/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-10-307ef.appspot.com/o/Convite%20especial%20Sophia%20XV%20anos.jpg?alt=media&token=e7a423fa-8837-4803-afee-ea9606d87dfb"  />
+            </div>
+        </main>
+    )
+    
+}
