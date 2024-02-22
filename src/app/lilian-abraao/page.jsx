ee742a9912a94ff85dac62698dd439f1b9cb5d38
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-02-22 16:41:36 -0300

    adicionando pagina de lilian-abraao

---
+++ src/app/lilian-abraao/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="lilian-abraao.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-1.appspot.com/o/lilian-abra%C3%A3o.mp4?alt=media&token=33c393a0-434b-4d50-9f6f-1a6c8df1dc37" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
