42221dd7a54cc57619a6ebf2f882cfd7749c423e
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-03-01 21:19:27 -0300

    adicionando pagina francielly-everton

---
+++ src/app/francielly-everton/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="francielly-everton.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-2.appspot.com/o/Convite%20especial%20Francielly%20e%20Everton%20(1).mp4?alt=media&token=020a7885-03ac-4692-ac17-7e71b084d0d1" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
