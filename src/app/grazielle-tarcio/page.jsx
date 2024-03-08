740c03340e37562d7ce6c2f163501a384f4d5141
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-03-08 07:41:57 -0300

    atualizando pagina grazielle-tarcio

--- src/app/grazielle-tarcio/page.jsx
+++ src/app/grazielle-tarcio/page.jsx
@@ -6,8 +6,8 @@ export default function Home() {
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
                 <video controls poster="grazielle-tarcio.jpg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-3.appspot.com/o/Convite%20especial%20Grazielle%20e%20Tarcio.mp4?alt=media&token=ffb6ad70-059d-49fd-a2b3-f00fc9e328eb" type="video/mp4" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-4.appspot.com/o/Convite%20especial%20Grazielle%20e%20Tarcio.mp4?alt=media&token=d73482fd-e7dc-4fbb-8222-60cd4ace91ed" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-3.appspot.com/o/Convite%20especial%20Grazielle%20e%20Tarcio%20(2).mp4?alt=media&token=d5c43a28-67b9-41ec-9764-9f3f4b40745d" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-4.appspot.com/o/Convite%20especial%20Grazielle%20e%20Tarcio%20(2).mp4?alt=media&token=39c602da-198f-4f10-b47a-b715a289e3cc" type="video/mp4" />
                 </video>    
             </div>
         </main>
