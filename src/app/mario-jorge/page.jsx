57b91fa1586d07512d5421ceb5649f1304ba8755
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-03-18 14:26:07 -0300

    criando pagina mario jorge

---
+++ src/app/mario-jorge/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="mariojorge.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-6.appspot.com/o/Convite%20especial%20Mario%20Jorge.mp4?alt=media&token=b04ec09c-d3b4-436a-abae-b89b45c65017" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
