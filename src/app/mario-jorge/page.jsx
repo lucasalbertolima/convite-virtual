08ea8a3114b1dcd7dc8d9ec4cc009e30711167fc
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-03-18 16:42:11 -0300

    alterando pagina mario jorge

--- src/app/mario-jorge/page.jsx
+++ src/app/mario-jorge/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
                 <video controls poster="mariojorge.jpg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-6.appspot.com/o/Convite%20especial%20Mario%20Jorge.mp4?alt=media&token=b04ec09c-d3b4-436a-abae-b89b45c65017" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-6.appspot.com/o/Convite%20especial%20Mario%20Jorge%20(1).mp4?alt=media&token=c3cab97d-2cda-4735-b6bd-db65b6ca8e5a" type="video/mp4" />
                 </video>    
             </div>
         </main>
