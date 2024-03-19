6b5783a70913014ad0d566732f614355f32c4049
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-03-19 13:11:14 -0300

    subindo paginas para serem editadas

---
+++ src/app/maria-liz/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="mariojorge.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-6.appspot.com/o/Convite%20especial%20Mario%20Jorge%20(1).mp4?alt=media&token=c3cab97d-2cda-4735-b6bd-db65b6ca8e5a" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
