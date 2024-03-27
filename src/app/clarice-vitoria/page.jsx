dbc0594c194b9e4f7a578276b835c436d6109f88
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-03-27 17:25:49 -0300

    adicionando pagina clarice-vitoria

---
+++ src/app/clarice-vitoria/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="clarice-vitoria.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-7.appspot.com/o/Convite%20especial%20Clarice%20Vit%C3%B3ria%20.mp4?alt=media&token=aef7fd44-f823-4630-a3e9-ad6209fe41fd" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
