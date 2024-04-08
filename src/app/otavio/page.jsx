ec52a1bde581b675abd3fc0248803c245d01ccfe
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-04-08 10:33:19 -0300

    subindo pagina otavio

---
+++ src/app/otavio/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="otavio.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-8.appspot.com/o/WhatsApp%20Video%202024-04-08%20at%2008.57.39.mp4?alt=media&token=ec87cf1f-c239-4c78-8664-2d3fb4f377db" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-9.appspot.com/o/WhatsApp%20Video%202024-04-08%20at%2008.57.39.mp4?alt=media&token=aba23e93-495d-4916-885a-9a032c29c44e" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
