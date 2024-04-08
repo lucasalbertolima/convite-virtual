21836c6c12cbcb14657ae421758080e53d64ce83
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-04-08 10:46:04 -0300

    subindo pagina maria carolina

---
+++ src/app/maria-carolina/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="maria-carolina.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-10.appspot.com/o/WhatsApp%20Video%202024-04-08%20at%2010.23.21.mp4?alt=media&token=52e1d499-d88a-48f4-8179-b6413d9524e2" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
