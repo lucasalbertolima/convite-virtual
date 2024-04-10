f159461e4e3aec20650338d49fc70966f58b72e3
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-04-10 20:28:22 -0300

    atualizando video maria carolina

--- src/app/maria-carolina/page.jsx
+++ src/app/maria-carolina/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
                 <video controls poster="maria-carolina.jpeg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-10.appspot.com/o/WhatsApp%20Video%202024-04-08%20at%2010.23.21.mp4?alt=media&token=52e1d499-d88a-48f4-8179-b6413d9524e2" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-10.appspot.com/o/Convite%20especial%20Maria%20Carolina.mp4?alt=media&token=0071af08-af8c-40cc-811a-dada3424ae61" type="video/mp4" />
                 </video>    
             </div>
         </main>
