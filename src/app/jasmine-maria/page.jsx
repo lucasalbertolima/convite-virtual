486ae0dd183e9debedb2e22c359348a70053b297
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-04-14 16:31:59 -0300

    subindo pagina jasmine-maria

---
+++ src/app/jasmine-maria/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="jasmine-maria.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/michelle-thiago.appspot.com/o/jasmine-maria.mp4?alt=media&token=7f542fa8-fac9-4e80-b3d9-5cd02e930302" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-mari-andre.appspot.com/o/jasmine-maria.mp4?alt=media&token=8ed1712e-993c-441b-b197-93351963634b" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
