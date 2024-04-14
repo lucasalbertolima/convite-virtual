f2fc6c31df6aa190f5882029d441a6339d029b97
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-04-13 23:11:20 -0300

    subindo segunda pagina digeorge

---
+++ src/app/digiorge-luiz/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="digiorge.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-12.appspot.com/o/digiorge-luiz.mp4?alt=media&token=bca9b7e6-5da3-4d6a-85da-dd16ed3461e6" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
