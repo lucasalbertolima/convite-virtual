824df5ca4695e8ea6cef6f3c50469a87f7fccdc8
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-04-13 21:46:47 -0300

    subindo pagina digeorge

---
+++ src/app/digiorge/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="digiorge.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-11.appspot.com/o/digiorge.mp4?alt=media&token=6ea2fc00-dcc1-4b65-a985-e2f5a2a8e3d1" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
