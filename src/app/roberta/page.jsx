0b6ccfd0f9b5147e4ad0c9423d6d493ca6cccfb3
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-04-17 12:54:01 -0300

    subindo pagina de victoria e roberta

---
+++ src/app/roberta/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="roberta.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-digital-bysarahjandira.appspot.com/o/roberta%20video.mp4?alt=media&token=ddfc5751-32d1-4b91-8dd5-80a330b0a546" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
---
+++ src/app/victoria-heloise/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="victoriaheloise.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/erismara-matteus.appspot.com/o/Convite%20Especial%20Victoria%20Heloise%20(1).mp4?alt=media&token=c0784fec-dd5f-47c0-8cab-d3aaf89abaf5" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-laura12.appspot.com/o/Convite%20Especial%20Victoria%20Heloise%20(1).mp4?alt=media&token=36a9e9cb-1ab9-43cd-9091-9d89841e125f" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
