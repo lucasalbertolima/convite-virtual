2f0a1996454891552c74c60ce64ca9007760f693
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-05-31 13:46:37 -0300

    testanto cardapio com imagem

--- src/app/cardapio-kyoto/page.jsx
+++ src/app/cardapio-kyoto/page.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-
 export default function Home() {
-  const pdfUrl = 'https://firebasestorage.googleapis.com/v0/b/convite-sarah-02.appspot.com/o/Kyoto%20Cozinha%20Oriental.pdf?alt=media&token=6ba70c03-e614-4913-ba10-66b357db3c98'; // Substitua pelo link do seu PDF
 
-  return (
-    <main>
-      <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
-        <embed src={pdfUrl} type="application/pdf" width="100%" height="100%" />
-      </div>
-    </main>
-  );
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <div className=" text-white h-full w-full bg-cardapio">
+                    <div className="flex flex-col h-full items-center justify-center pt-6">
+                        <a className="space-cardapio px-24" href="https://www.ifood.com.br/delivery/aracaju-se/kyoto---inacio-barbosa-inacio-barbosa/b8633329-8ca9-4a59-baf5-1833b744d0ad?utm_medium=share"></a>
+                        <a className="space-cardapio px-24 mt-4" href="https://api.whatsapp.com/send/?phone=5579996544046&text=Ol%C3%A1+Kyoto%2C+quero+fazer+meu+pedido.+&type=phone_number&app_absent=0"></a>
+                        <a className="space-cardapio px-24" href="https://www.google.com/maps/place/Villa+Gastr%C3%B4+Food/@-10.9553815,-37.0781571,17z/data=!3m1!4b1!4m6!3m5!1s0x71ab100537f7dc7:0x787382bec5fcf917!8m2!3d-10.9553815!4d-37.0781571!16s%2Fg%2F11vm9hnm14?entry=ttu"></a>
+                        <a className="space-cardapio px-24" href="https://www.instagram.com/kyotocozinhaoriental"></a>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+    
 }
