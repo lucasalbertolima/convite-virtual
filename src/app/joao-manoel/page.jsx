d42169110a272b460c57bfc99935d99d191f42a9
Lucas Alberto Dos Santos <lucas@MacBook-Air-de-Lucas.local>
2024-06-04 12:21:26 -0300

    subindo pagina de joao manoel no mac

---
+++ src/app/joao-manoel/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="joaomanoel.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-03.appspot.com/o/Convite%20Especial%20Fazendinha%20do%20Joa%CC%83o%20Manoel.mp4?alt=media&token=6f234650-355c-480c-80b5-023d7ce19cfe" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-04.appspot.com/o/Convite%20Especial%20Fazendinha%20do%20Joa%CC%83o%20Manoel.mp4?alt=media&token=567acafa-57c4-48c5-9a5d-59700339aea3" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
