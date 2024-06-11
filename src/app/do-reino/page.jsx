c284f647d93a9e71f904d89619c5425229998c83
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-06-11 20:58:02 -0300

    commit do reino

---
+++ src/app/do-reino/page.jsx
@@ -0,0 +1,18 @@
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <div className=" text-white h-full w-full bg-doreino">
+                    <div className="flex flex-col h-full items-center justify-center pt-6">
+                        <a className="space-doreino px-24" href="https://www.canva.com/design/DAF-y2bQXNU/2UvqrdNS1Q1FY402nMiwoA/view?utm_content=DAF-y2bQXNU&utm_campaign=designshare&utm_medium=link&utm_source=editor"></a>
+                        <a className="space-doreino px-24 mt-4" href="https://wa.me/message/ZGUAJGX3QPDXG1"></a>
+                        <a className="space-doreino px-24" href="https://maps.app.goo.gl/NZCSDyGsfoSSPjAe8"></a>
+                        <a className="space-doreino px-24" href="https://www.instagram.com/do_reino_079/"></a>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+    
+}
