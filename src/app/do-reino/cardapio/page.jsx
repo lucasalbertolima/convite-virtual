a1d7a54f0081c5674c5241800f285fb151b888d6
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-06-11 21:08:07 -0300

    commit do reino cardapio

---
+++ src/app/do-reino/cardapio/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-05.appspot.com/o/LANCHONETE%20DO%20REINO.jpg?alt=media&token=fb398bc3-5653-48a4-abfe-afc5a85d10f6"  />
+            </div>
+        </main>
+    )
+    
+}
