7a5951239578c8df6aefc527dd1b3d40adcc878e
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-06-20 19:36:56 -0300

    comit echt

---
+++ src/app/echt/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="echt.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-07.appspot.com/o/WhatsApp%20Video%202024-06-20%20at%2015.07.33.mp4?alt=media&token=4140ae05-050f-40ff-a347-0ec8f248793f" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-08.appspot.com/o/WhatsApp%20Video%202024-06-20%20at%2015.07.33.mp4?alt=media&token=dd2d54b9-3109-4909-9c3a-7fd658b3234b" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
