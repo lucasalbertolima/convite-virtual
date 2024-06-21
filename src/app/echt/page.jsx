2c6c1cb46a9de8561d0ed70cf9775e93ddd5fd4d
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-06-21 05:46:30 -0300

    atualizacao do video echt

--- src/app/echt/page.jsx
+++ src/app/echt/page.jsx
@@ -6,8 +6,8 @@ export default function Home() {
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
                 <video controls poster="echt.jpg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-07.appspot.com/o/WhatsApp%20Video%202024-06-20%20at%2015.07.33.mp4?alt=media&token=4140ae05-050f-40ff-a347-0ec8f248793f" type="video/mp4" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-08.appspot.com/o/WhatsApp%20Video%202024-06-20%20at%2015.07.33.mp4?alt=media&token=dd2d54b9-3109-4909-9c3a-7fd658b3234b" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-08.appspot.com/o/WhatsApp%20Video%202024-06-20%20at%2023.13.48.mp4?alt=media&token=6c1b0822-b38a-418c-92cf-97eb7d334ebe" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-07.appspot.com/o/WhatsApp%20Video%202024-06-20%20at%2023.13.48.mp4?alt=media&token=51d52c91-59aa-4c03-884a-c1c87f05e452" type="video/mp4" />
                 </video>    
             </div>
         </main>
