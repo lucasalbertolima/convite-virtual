f6f2705aa2e2628bd449146fdf724a4c3f8b0f59
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-06-23 13:01:19 -0300

    subindo pagina maria laura

---
+++ src/app/maria-laura/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="maria-laura.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-09.appspot.com/o/WhatsApp%20Video%202024-06-23%20at%2012.38.07.mp4?alt=media&token=0572a3c7-af7c-4f71-9aaa-61fa656e0db3" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
