dd415a4a50b17f01b7135de73082c2959925c150
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-06-30 16:57:20 -0300

    subindo pagina de presente da pagina fazendinha maria liz

--- src/app/maria-liz-fazendinha/presente/page.jsx
+++ src/app/maria-liz-fazendinha/presente/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     return(
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
-                    <img src="https://firebasestorage.googleapis.com/v0/b/phd-global.appspot.com/o/Convite%20Especial%20Daniela.mp4?alt=media&token=3c567f87-e79a-40be-800d-7a2b6215688b"  />
+                    <img src="https://firebasestorage.googleapis.com/v0/b/phd-global.appspot.com/o/Convite%20especial%20maria%20Liz.jpg?alt=media&token=e7110dff-606b-47a9-ad11-7926c424a2fd"  />
             </div>
         </main>
     )
