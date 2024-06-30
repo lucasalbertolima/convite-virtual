052f2b34ae3f2feb0fe8e3ccd5e15e3cb9d9bae4
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-06-30 14:52:33 -0300

    alterando imagem da pagina fazendinha maria liz

--- src/app/maria-eduarda/page.jsx
+++ src/app/maria-eduarda/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     return(
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
-                <video controls poster="maria-eduarda.jpeg">
+                <video controls poster="maria-liz-fazendinha.jpg">
                     <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-videos.appspot.com/o/Convite%20especial%20maria%20Liz.mp4?alt=media&token=5f2593e2-4ec3-4427-b7e9-b5c0bb4dd382" type="video/mp4" />
                     <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-3.appspot.com/o/Convite%20especial%20maria%20Liz.mp4?alt=media&token=3ffe4e03-58db-4ed0-98c3-25529afa5f84" type="video/mp4" />
                 </video>    
