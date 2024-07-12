d5b04018a120cd9b2d911af4c33208a4e7bb1209
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-07-12 10:40:50 -0300

    alteração no video de barbara

--- src/app/barbara-iglesse/page.jsx
+++ src/app/barbara-iglesse/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
                 <video controls poster="barbara-iglesse.jpeg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-01.appspot.com/o/barbara-iglesse.mp4?alt=media&token=5f92120a-11b3-46ae-b344-5dfe086189bd" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-01.appspot.com/o/Convite%20v%C3%ADdeo%20azul%20B%C3%A1rbara%20e%20Iglesse%20%20(1).mp4?alt=media&token=1f42c870-eea9-43ae-b628-d0cdaeb686c1" type="video/mp4" />
                 </video>    
             </div>
         </main>
