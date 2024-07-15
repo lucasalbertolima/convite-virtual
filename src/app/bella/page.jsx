476a6d3cef0ed4457a246fffe163ef7f66e01d6d
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-07-15 11:04:04 -0300

    subindo pagina bella

---
+++ src/app/bella/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="bella.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convitesarah-10.appspot.com/o/Convite%20especial%20Bella.mp4?alt=media&token=4454caeb-fdef-4c92-98c6-9a295052d7c5" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
