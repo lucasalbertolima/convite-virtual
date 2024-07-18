100870fd8d164d8ea7502aef447c1d960df9e72c
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-07-18 12:45:23 -0300

    subindo pagina maria-flor

---
+++ src/app/maria-flor/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="maria-flor.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convitesarah-11.appspot.com/o/maria-flor.mp4?alt=media&token=6e0c35ae-ed8c-4113-a788-9970f5f6d6d7" type="video/mp4" />
+                </video> 
+            </div>
+        </main>
+    )
+    
+}
