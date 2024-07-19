b2d46e0fb39a7fb302c378f424f5ac9384aa3014
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-07-19 14:38:03 -0300

    subindo pagina para presente de roseane

---
+++ src/app/roseane-presente/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/lucasdesenweb.appspot.com/o/Convite%20Especial%20Roseane%20e%20Cley.jpg?alt=media&token=964fbbc5-ed9a-44ea-a145-d16ccd53a4ba"  />
+            </div>
+        </main>
+    )
+    
+}
