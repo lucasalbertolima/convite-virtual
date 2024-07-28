dd738748cfe408a1cc154db0a742f62927b4b5bd
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-07-27 22:22:28 -0300

    subindo pagina de thamires

---
+++ src/app/thamires-genilson/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="thamires-genilson.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-12-22086.appspot.com/o/thamires-genilson.mp4?alt=media&token=6177c5b9-a0b9-4a08-9666-ce2cef003707" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
