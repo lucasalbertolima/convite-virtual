c520eabb7d3da4f661e52669b90de2e9b81e4222
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-08-09 12:09:41 -0300

    corrindo pagina de bryan

--- src/app/bryan/page.jsx
+++ src/app/bryan/page.jsx
@@ -5,10 +5,10 @@ export default function Home() {
     return(
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
-                <video controls poster="mario-jorge-dino.jpeg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-2.appspot.com/o/C%C3%B3pia%20de%20Convite%20especial%20Mario%20Jorge.mp4?alt=media&token=3ac6d77e-02dd-4d0a-b2b8-93233ae5ffc2" type="video/mp4" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-3.appspot.com/o/C%C3%B3pia%20de%20Convite%20especial%20Mario%20Jorge.mp4?alt=media&token=fbb05b14-db05-4db9-bdc0-0c2f63e28e46" type="video/mp4" />
-                </video>    
+                <video controls poster="bryan.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-9.appspot.com/o/WhatsApp%20Video%202024-08-09%20at%2011.49.46.mp4?alt=media&token=b2f318a5-343f-412d-9962-4d14d6ebf319" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-10.appspot.com/o/WhatsApp%20Video%202024-08-09%20at%2011.49.46.mp4?alt=media&token=414a1307-e8b8-4df7-a042-023901618702" type="video/mp4" />
+                </video>     
             </div>
         </main>
     )
