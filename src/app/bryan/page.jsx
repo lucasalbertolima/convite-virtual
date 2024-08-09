70d65367e8ccfa32755bcae44c32e6ebc8787b68
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-08-09 12:07:35 -0300

    subindo pagina de bryan

---
+++ src/app/bryan/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="mario-jorge-dino.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-2.appspot.com/o/C%C3%B3pia%20de%20Convite%20especial%20Mario%20Jorge.mp4?alt=media&token=3ac6d77e-02dd-4d0a-b2b8-93233ae5ffc2" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-3.appspot.com/o/C%C3%B3pia%20de%20Convite%20especial%20Mario%20Jorge.mp4?alt=media&token=fbb05b14-db05-4db9-bdc0-0c2f63e28e46" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
