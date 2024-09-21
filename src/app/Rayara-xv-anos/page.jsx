c7fa9b8ee51221518d21c8dd2aab9f57678a6416
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-09-21 20:52:38 -0300

    subindo pagina de rayara

---
+++ src/app/Rayara-xv-anos/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="Rayara-xv-anos.jpg.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-5-cabe1.appspot.com/o/WhatsApp%20Video%202024-09-21%20at%2013.03.54.mp4?alt=media&token=4e708b1e-20d5-419d-a96e-9cf415f0f721" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
---
+++ src/app/Rayara-xv-anos/presente/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-5-cabe1.appspot.com/o/Convite%20Especial%20Rayara%20xv%20anos%202.jpg.jpg?alt=media&token=3f915479-7ba9-41e1-b480-1a9d80414973"  />
+            </div>
+        </main>
+    )
+    
+}
