ba5abdca0d86a6a43097f538c59052e1aa3c9eb4
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-09-26 11:31:56 -0300

    alteracao no video de rayara

--- src/app/Rayara-xv-anos/page.jsx
+++ src/app/Rayara-xv-anos/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
                 <video controls poster="Rayara-xv-anos.jpg.jpg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-5-cabe1.appspot.com/o/WhatsApp%20Video%202024-09-21%20at%2013.03.54.mp4?alt=media&token=4e708b1e-20d5-419d-a96e-9cf415f0f721" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/phd-global.appspot.com/o/rayara.mp4?alt=media&token=d909e2d5-fad7-4a21-b568-e7aeaf7ea9d7" type="video/mp4" />
                 </video>    
             </div>
         </main>
