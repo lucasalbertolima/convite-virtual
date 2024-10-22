5ecb928e1de59ad19480e3fb4b9120eb0c013a17
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-10-21 22:32:05 -0300

    subindo pagina de presente de brendha

---
+++ src/app/brendha/presente/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-5.appspot.com/o/sugestao%20brendha.jpeg?alt=media&token=94a89422-d865-4760-9457-c6d5736b8237"  />
+            </div>
+        </main>
+    )
+    
+}
