ade3f43e785318862af10c88e6bceaeacdf830d5
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-10-21 22:24:00 -0300

    subindo pagina brendha

---
+++ src/app/brendha/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="brendha.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-4.appspot.com/o/convite%20video%20brendha.mp4?alt=media&token=078f2901-1477-4242-99e2-cf3c81d9c712" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-5.appspot.com/o/convite%20video%20brendha.mp4?alt=media&token=050a922b-0678-40ed-a3b4-3854d901fbad" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
