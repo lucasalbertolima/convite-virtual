639737aa3beea3320562dc6b612de241b9d5088e
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-12-03 17:22:20 -0300

    subindo pagina anne beatriz

---
+++ src/app/anne-beatriz/page.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="anne-beatriz.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-3-de741.appspot.com/o/Anne%20beatriz%20video.mp4?alt=media&token=1f455da7-a013-469a-b31c-7bd2f940708a" type="video/mp4" />
+                </video>     
+            </div>
+        </main>
+    )
+    
+}
