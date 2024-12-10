2ad2ccf1984c57e2add4dde06953d2a472a232e3
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-12-10 17:12:24 -0300

    subindo a pagina kaliane-antonio

---
+++ src/app/kaliane-antonio/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="kaliane-antonio.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-8-e213a.appspot.com/o/Convite%20Especial%20Kaliane%20e%20Ant%C3%B4nio%20video%20(1).mp4?alt=media&token=0f6b73ed-051d-4a8c-8f7e-a903957849bd" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-9-fff65.appspot.com/o/Convite%20Especial%20Kaliane%20e%20Ant%C3%B4nio%20video%20(1).mp4?alt=media&token=c50c8b4c-1fc5-4d32-926e-916a981fc597" type="video/mp4" />
+                </video>   
+            </div>
+        </main>
+    )
+    
+}
