062be2e8f3f3ea9635c93a5ea5b52a0756ba093e
lucasalbertolima <lucas.alberto.san@gmail.com>
2024-12-12 15:47:14 -0300

    subindo a pagina CECILIA-BRANDAO

---
+++ src/app/cecilia-brandao/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="cecilia-brandao.png">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-10-307ef.appspot.com/o/Convite%20especial%20Cecilia%20Brandao%20video.mp4?alt=media&token=4df2117a-df23-454a-860f-a1f0b024cc2a" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-11-af5bd.appspot.com/o/Convite%20especial%20Cecilia%20Brandao%20video.mp4?alt=media&token=b1ab0e87-7733-42be-b685-22718aff59c2" type="video/mp4" />
+                </video>   
+            </div>
+        </main>
+    )
+    
+}
