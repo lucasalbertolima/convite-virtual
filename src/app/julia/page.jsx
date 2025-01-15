f10fbf80fe7ec5c4e01e1e9f305a60aeb23cfaaf
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-01-14 23:09:13 -0300

    subindo imagem julia

--- src/app/julia/page.jsx
+++ src/app/julia/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     return(
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
-                <video controls poster="monik.jpg">
+                <video controls poster="juliaimagem.jpeg">
                     <source src="https://firebasestorage.googleapis.com/v0/b/apinoticias-with-next.appspot.com/o/julia-video.mp4?alt=media&token=53aebceb-baa6-489d-8a96-375823985ddd" type="video/mp4" />
                 </video>   
             </div>
