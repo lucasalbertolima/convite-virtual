bf9c343a00e27562c8aaf99593c030ff4e533943
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-01-15 21:27:36 -0300

    subindo pagina de jade-2anos

---
+++ src/app/jade-2anos/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="jade-2anos.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/o-ano-de-ouro-do-theo.appspot.com/o/jade-2anos.mp4?alt=media&token=ce91ee75-3a26-4123-a3b1-6e636dbaa530" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-by-sarahjandira.appspot.com/o/jade-2anos.mp4?alt=media&token=0f027dd1-056a-4c1e-a58c-b629217ff38c" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
