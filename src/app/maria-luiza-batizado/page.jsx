2d7eab3c0d4dda2200248dff419bf6d7318ecb56
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-01-29 08:36:54 -0300

    subindo pagina maria-luiza-batizado

---
+++ src/app/maria-luiza-batizado/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="marialuizabatizado.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-8-e213a.appspot.com/o/Convite%20Especial%20Maria%20Luiza.mp4?alt=media&token=f15e5967-d1cf-4aed-af5d-ac5f9e9053df" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-9-fff65.appspot.com/o/Convite%20Especial%20Maria%20Luiza.mp4?alt=media&token=2882b1f0-2103-411b-b2e7-726d16724a82" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
