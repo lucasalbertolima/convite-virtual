8c49547d598352ea4f4bf723cabcc2675daab11f
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-01-29 10:16:28 -0300

    subindo pagina marlene-angelo

---
+++ src/app/marlene-angelo/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="marle-angelo.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-10-307ef.appspot.com/o/_Convite%20Especial%20Amanda%20e%20Bruno.mp4?alt=media&token=37141d06-8cb2-4333-807c-7efb9853f47c" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-11-af5bd.appspot.com/o/_Convite%20Especial%20Amanda%20e%20Bruno.mp4?alt=media&token=b16cce0c-327b-468f-82cb-f24f674a7da1" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
