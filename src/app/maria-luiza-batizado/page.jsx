925223b4433ea9740594d8f4c9eea7649d63016f
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-01-30 16:48:26 -0300

    alterando video de maria-luiza-batizado

--- src/app/maria-luiza-batizado/page.jsx
+++ src/app/maria-luiza-batizado/page.jsx
@@ -6,8 +6,8 @@ export default function Home() {
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
                 <video controls poster="marialuizabatizado.jpg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-8-e213a.appspot.com/o/Convite%20Especial%20Maria%20Luiza.mp4?alt=media&token=f15e5967-d1cf-4aed-af5d-ac5f9e9053df" type="video/mp4" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-9-fff65.appspot.com/o/Convite%20Especial%20Maria%20Luiza.mp4?alt=media&token=2882b1f0-2103-411b-b2e7-726d16724a82" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-9-fff65.appspot.com/o/Convite%20Especial%20Maria%20Luiza%20(1).mp4?alt=media&token=f86e171a-452d-4dc7-a7aa-85ef8405542c" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-8-e213a.appspot.com/o/Convite%20Especial%20Maria%20Luiza%20(1).mp4?alt=media&token=3da1d7fe-190c-4e1b-b5b0-2d99f64259cd" type="video/mp4" />
                 </video>    
             </div>
         </main>
