4ead49b0165dac4aa902275c42423df3ba39253d
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-02-11 15:10:39 -0300

    subindo pagina de hugo e luana

---
+++ src/app/hugo-luana/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="hugo-luana.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-3.appspot.com/o/Convite%20Especial%20Luana%20e%20Hugo%20(1).mp4?alt=media&token=8d0916be-5fa7-479d-b2d8-5b265ae11f71" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-4.appspot.com/o/Convite%20Especial%20Luana%20e%20Hugo%20(1).mp4?alt=media&token=8f813851-25b5-46dc-974b-053e4ce58f77" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
---
+++ src/app/hugo-luana/presente/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-5.appspot.com/o/Convite%20Especial%20Luana%20e%20Hugo%20(1).jpg?alt=media&token=88087043-a8f6-4be3-909e-ddc630fb3096"  />
+            </div>
+        </main>
+    )
+    
+}
