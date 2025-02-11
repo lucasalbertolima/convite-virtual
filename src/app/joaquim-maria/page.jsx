78aaccf12bf07600e96c974dfce92f119e65c692
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-02-11 17:11:37 -0300

    alterando video de joaquim e maria

--- src/app/joaquim-maria/page.jsx
+++ src/app/joaquim-maria/page.jsx
@@ -6,8 +6,8 @@ export default function Home() {
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
                 <video controls poster="joaquim-maria.png">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-11.appspot.com/o/Joaquim%20e%20Maria.mp4?alt=media&token=32793950-7eb9-48ba-aafd-a708a860f08f" type="video/mp4" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-10.appspot.com/o/Joaquim%20e%20Maria.mp4?alt=media&token=934d8e21-e360-481b-9cc4-95bda655be4b" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-11.appspot.com/o/Convite%20Especial%20Joaquim%20e%20Maria%20(1).mp4?alt=media&token=7712a1f9-9a7c-41be-9b8c-0f98c6f8f85f" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-10.appspot.com/o/Convite%20Especial%20Joaquim%20e%20Maria%20(1).mp4?alt=media&token=fe8ccd7a-bc30-43ea-882a-034b991fe9fe" type="video/mp4" />
                 </video>    
             </div>
         </main>
