0a472b4171be09a815cae7b0a38e3d0ff2cc6d90
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-02-18 18:26:23 -0300

    alterando video de larissa-alan

--- src/app/larissa-alan/page.jsx
+++ src/app/larissa-alan/page.jsx
@@ -6,8 +6,8 @@ export default function Home() {
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
                 <video controls poster="larissa-alan.jpg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-6.appspot.com/o/Convite%20Especial%20Larissa%20e%20Alan.mp4?alt=media&token=111d3677-cf2a-4b2d-8c31-fefd9460184b" type="video/mp4" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-7.appspot.com/o/Convite%20Especial%20Larissa%20e%20Alan.mp4?alt=media&token=f876c035-bfb2-45ec-b559-338ed98757e9" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-6.appspot.com/o/Convite%20Especial%20Larissa%20e%20Alan.mp4?alt=media&token=bfa50c1d-7c5f-42ce-83f9-0e2200e716ae" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-7.appspot.com/o/Convite%20Especial%20Larissa%20e%20Alan.mp4?alt=media&token=d54ab268-259f-4a93-9403-d3fc6f3f26e2" type="video/mp4" />
                 </video>    
             </div>
         </main>
