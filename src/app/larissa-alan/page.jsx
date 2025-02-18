9eb44e06724006172cc4cda5c04e7449f16dfdc4
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-02-18 13:20:49 -0300

    subindo pagina larissa-alan

---
+++ src/app/larissa-alan/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="hugo-luana.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-6.appspot.com/o/Convite%20Especial%20Larissa%20e%20Alan.mp4?alt=media&token=111d3677-cf2a-4b2d-8c31-fefd9460184b" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-7.appspot.com/o/Convite%20Especial%20Larissa%20e%20Alan.mp4?alt=media&token=f876c035-bfb2-45ec-b559-338ed98757e9" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
---
+++ src/app/larissa-alan/presente/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-6.appspot.com/o/Convite%20Especial%20Larissa%20e%20Alan.png?alt=media&token=9c29c1d1-48bc-48ac-a328-5f8f4bca58da"  />
+            </div>
+        </main>
+    )
+    
+}
