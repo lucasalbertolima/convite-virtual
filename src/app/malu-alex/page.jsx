9f327388e187b397e8b13278b7b3032635168b8d
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-02-24 00:00:05 -0300

    alterando video malu-alex

--- src/app/malu-alex/page.jsx
+++ src/app/malu-alex/page.jsx
@@ -4,8 +4,8 @@ export default function Home() {
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
                 <video controls poster="malu-alex.jpg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-videos.appspot.com/o/Convite%20Especial%20Malu%20e%20Alex.mp4?alt=media&token=424a8d9d-e9d8-48a7-8a43-1627be1453f1" type="video/mp4" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-videos-2.appspot.com/o/Convite%20Especial%20Malu%20e%20Alex.mp4?alt=media&token=d43e8832-db2d-44e3-aa94-34964cc68224" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-videos.appspot.com/o/malu-alex.mp4?alt=media&token=9640ec83-784b-4998-808e-ade70ca360bc" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-videos-2.appspot.com/o/malu-alex.mp4?alt=media&token=188250b0-7d42-4c75-8680-ea26b4e1645c" type="video/mp4" />
                 </video>    
             </div>
         </main>
