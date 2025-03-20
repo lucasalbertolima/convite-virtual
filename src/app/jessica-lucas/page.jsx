7298327eba33042aa9be24fbe465d3a13ad5c4aa
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-03-20 18:39:01 -0300

    alterando pagina jessica-lucas

--- src/app/jessica-lucas/page.jsx
+++ src/app/jessica-lucas/page.jsx
@@ -5,9 +5,9 @@ export default function Home() {
     return(
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
-                <video controls poster="daniela.jpg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/phd-global.appspot.com/o/Convite%20Especial%20Daniela.mp4?alt=media&token=3c567f87-e79a-40be-800d-7a2b6215688b" type="video/mp4" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/o-ano-de-ouro-do-theo.appspot.com/o/Convite%20Especial%20Daniela.mp4?alt=media&token=9e0e5112-2b1c-4484-9624-6f19761e59a1" type="video/mp4" />
+                <video controls poster="jessica-lucas.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/michelle-thiago.appspot.com/o/jessica-lucas.mp4?alt=media&token=1172634c-87ad-41f6-a1d9-1337ef7f7821" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/erismara-matteus.appspot.com/o/jessica-lucas.mp4?alt=media&token=a8b27b1e-9f07-4ecb-8c7f-20f84a04a4fb" type="video/mp4" />
                 </video>    
             </div>
         </main>
