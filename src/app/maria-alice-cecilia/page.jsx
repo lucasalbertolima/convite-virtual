00c7f32d1d44d5f662c13f66b8a4c6575b2272f2
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-03-24 18:23:52 -0300

    subindo pagina maria-alice-cecilia

---
+++ src/app/maria-alice-cecilia/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="maria-alice-cecilia.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-sarah-09.appspot.com/o/Convite%20Especial%20Maria%20Alice%20%26%20Maria%20Ceci%CC%81lia%20(1).mp4?alt=media&token=7eccc0b6-4c08-4789-a0b5-f4df652157ba" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convitesarah-10.appspot.com/o/Convite%20Especial%20Maria%20Alice%20%26%20Maria%20Ceci%CC%81lia%20(1).mp4?alt=media&token=1fe1b76f-0cd3-4948-b886-2d3c5ee2c550" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
