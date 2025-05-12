4092da9a245c348589bdefb0875b8419107a5cc3
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-05-12 14:39:37 -0300

    alterando video luana-hugo

--- src/app/hugo-luana/page.jsx
+++ src/app/hugo-luana/page.jsx
@@ -6,8 +6,8 @@ export default function Home() {
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
                 <video controls poster="hugo-luana.jpg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-3.appspot.com/o/Convite%20Especial%20Luana%20e%20Hugo.mp4?alt=media&token=43aaede8-3e5d-4b5d-b71f-495a81451e62" type="video/mp4" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-4.appspot.com/o/Convite%20Especial%20Luana%20e%20Hugo.mp4?alt=media&token=32e92480-9b02-41fb-8893-464916bc995a" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-3.appspot.com/o/hugo-luana.mp4?alt=media&token=9ac70836-92e6-4346-b591-6835a1d89da9" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-4.appspot.com/o/hugo-luana.mp4?alt=media&token=df86a798-f06d-4471-9f14-012d4444bbc2" type="video/mp4" />
                 </video>    
             </div>
         </main>
