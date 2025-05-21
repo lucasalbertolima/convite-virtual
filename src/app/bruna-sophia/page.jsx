eeda0fcf9a2f3353738757ec886bcea8169b3434
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-05-21 11:42:22 -0300

    subindo pagina bruna-sophia

---
+++ src/app/bruna-sophia/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="bruna-sophia.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-mari-andre.appspot.com/o/Convite%20especial%20Bruna%20Sophia.mp4?alt=media&token=34399d7e-aad9-4a88-a668-fa424522772b" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convitevirtual-7554a.appspot.com/o/Convite%20especial%20Bruna%20Sophia.mp4?alt=media&token=1b2c89a9-2336-4cd4-9182-42cec53ea871" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
