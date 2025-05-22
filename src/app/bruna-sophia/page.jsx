03a4cfa940004f7d1542dd83c4634a8edd389414
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-05-22 12:24:09 -0300

    alterando video e imagem bruna-sophia

--- src/app/bruna-sophia/page.jsx
+++ src/app/bruna-sophia/page.jsx
@@ -5,9 +5,9 @@ export default function Home() {
     return(
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
-                <video controls poster="bruna-sophia.jpg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-mari-andre.appspot.com/o/Convite%20especial%20Bruna%20Sophia.mp4?alt=media&token=34399d7e-aad9-4a88-a668-fa424522772b" type="video/mp4" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convitevirtual-7554a.appspot.com/o/Convite%20especial%20Bruna%20Sophia.mp4?alt=media&token=1b2c89a9-2336-4cd4-9182-42cec53ea871" type="video/mp4" />
+                <video controls poster="bruna-sophia.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-mari-andre.appspot.com/o/Bruna%20Sophia.mp4?alt=media&token=8450d30a-43eb-4b80-968c-9761ea61cafc" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convitevirtual-7554a.appspot.com/o/Bruna%20Sophia.mp4?alt=media&token=52e16937-51c3-4b56-9826-b0aa8d332c7f" type="video/mp4" />
                 </video>    
             </div>
         </main>
