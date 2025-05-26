ac7e48353cc396149273e95cb7daf91c2be1641d
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-05-26 12:39:58 -0300

    alterando imagem da pagina de presente de davi

--- src/app/davi/presente/page.jsx
+++ src/app/davi/presente/page.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     return(
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
-                    <img src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-videos-2.appspot.com/o/davi-presente.png?alt=media&token=71a3e744-352a-4b45-85bc-531c99300152"  />
+                    <img src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-videos-2.appspot.com/o/WhatsApp%20Image%202025-05-26%20at%2012.18.40.jpeg?alt=media&token=e587621f-2aaa-4492-9060-8898f0e1f240"  />
             </div>
         </main>
     )
