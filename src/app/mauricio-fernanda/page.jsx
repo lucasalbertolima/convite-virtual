42ee1a61fe18ae764d7dda71701813660b57290c
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-07-02 12:33:34 -0300

    subindo pagina mauricio-fernanda

---
+++ src/app/mauricio-fernanda/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="mauricio-fernanda.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/erismara-matteus.appspot.com/o/Convite%20Especial%20Mauri%CC%81cio%20e%20Fernanda%20(2).mp4?alt=media&token=cc93f065-9e71-4826-82ef-3733bffc0612" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/phd-global.appspot.com/o/Convite%20Especial%20Mauri%CC%81cio%20e%20Fernanda%20(2).mp4?alt=media&token=28221250-2c9e-4e46-871e-0a8d2975619a" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
