70430fadb48b13441941dca158ff46b3e2d0a867
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-07-11 13:29:08 -0300

    subindo cleiton taiane

---
+++ src/app/cleiton-taiane/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="cris-luana.png">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/o-ano-de-ouro-do-theo.appspot.com/o/Convite%20Especial%20C%26L.mp4?alt=media&token=8bf66d31-c51a-41e7-b6b2-2ef9d89a883d" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/michelle-thiago.appspot.com/o/Convite%20Especial%20C%26L.mp4?alt=media&token=e59ef756-8a34-474a-b2f5-2f71ca9c668c" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
