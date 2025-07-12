a4b04fb82c381e924335f6a41f433e740688f3f8
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-07-11 22:54:59 -0300

    alterando video cleiton-taiane

--- src/app/cleiton-taiane/page.jsx
+++ src/app/cleiton-taiane/page.jsx
@@ -5,9 +5,9 @@ export default function Home() {
     return(
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
-                <video controls poster="cris-luana.png">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/o-ano-de-ouro-do-theo.appspot.com/o/Convite%20Especial%20C%26L.mp4?alt=media&token=8bf66d31-c51a-41e7-b6b2-2ef9d89a883d" type="video/mp4" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/michelle-thiago.appspot.com/o/Convite%20Especial%20C%26L.mp4?alt=media&token=e59ef756-8a34-474a-b2f5-2f71ca9c668c" type="video/mp4" />
+                <video controls poster="taiane-cleiton.png">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-9-fff65.appspot.com/o/Convite%20Especial%20Taiane%20e%20Cleiton.mp4?alt=media&token=b511f951-b82e-4ee5-b4ca-d9b355c49220" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-6-6f46d.appspot.com/o/Convite%20Especial%20Taiane%20e%20Cleiton.mp4?alt=media&token=d2238ea0-d243-4dad-b7d8-05127bff4c28" type="video/mp4" />
                 </video>    
             </div>
         </main>
