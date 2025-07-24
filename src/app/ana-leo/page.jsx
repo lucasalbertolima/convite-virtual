860b13b0309cd3d974680565618dcc50164f173f
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-07-24 10:50:44 -0300

    subindo pagina ana-leo

---
+++ src/app/ana-leo/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="ana-leo.jpeg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-11-af5bd.appspot.com/o/Convite%20Especial%20Le%CC%81o%20e%20Ana%20(1).mp4?alt=media&token=277fddee-8b7d-4292-965a-affe944de774" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-5-cabe1.appspot.com/o/Convite%20Especial%20Le%CC%81o%20e%20Ana%20(1).mp4?alt=media&token=868d01d0-bebe-499c-ba3c-1f1a98a6f2f8" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
