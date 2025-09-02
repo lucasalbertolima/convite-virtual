039acfdb50c0d376e88595a454cd03c6a83d0016
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-09-02 09:24:29 -0300

    alterando informaçoes gabrielle-maria

---
+++ src/app/gabriella-renan-copia/page.jsx
@@ -0,0 +1,16 @@
+
+
+export default function Home() {
+
+    return(
+        <main>
+            <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
+                <video controls poster="gabriella-renan.jpg">
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-7-441ce.appspot.com/o/Convite%20Especial%20Gabriella%20%26%20Renan%20(1).mp4?alt=media&token=c3d5f1e7-33fe-4841-a879-61fb733f305c" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-8-e213a.appspot.com/o/Convite%20Especial%20Gabriella%20%26%20Renan%20(1).mp4?alt=media&token=a4c7dcf9-ba45-424c-bb90-aba9ca3e9089" type="video/mp4" />
+                </video>    
+            </div>
+        </main>
+    )
+    
+}
