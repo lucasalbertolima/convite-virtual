1d5786d30e156ec9de8369cae9cbc7071cafd612
lucasalbertolima <lucas.alberto.san@gmail.com>
2025-09-02 09:08:31 -0300

    subindo pagina gabrielle-maria

--- src/app/gabrielle-maria/page.jsx
+++ src/app/gabrielle-maria/page.jsx
@@ -6,8 +6,8 @@ export default function Home() {
         <main>
             <div className="w-full h-screen lg:w-1/4 lg:mx-auto lg:flex lg:justify-center lg:items-center">
                 <video controls poster="gabrielle-maria.jpg">
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-3-de741.appspot.com/o/IMG_3472.MOV?alt=media&token=8a4d9ec2-f7cd-4a36-8cb4-247ffa83326f" type="video/mp4" />
-                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-2.appspot.com/o/IMG_3472.MOV?alt=media&token=7a06da2b-1696-4e23-82a2-9c90693ab1f5" type="video/mp4" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-3-de741.appspot.com/o/IMG_3472.MOV?alt=media&token=8a4d9ec2-f7cd-4a36-8cb4-247ffa83326f" type="video/mov" />
+                    <source src="https://firebasestorage.googleapis.com/v0/b/convite-virtual-2.appspot.com/o/IMG_3472.MOV?alt=media&token=7a06da2b-1696-4e23-82a2-9c90693ab1f5" type="video/mov" />
                 </video>    
             </div>
         </main>
